fb0a2d8f540cadd086e27025640d01a46c2d147a
Ítalo Béllo <116759831+Lflickr@users.noreply.github.com>
2024-10-21 15:22:22 -0300

    Add files via upload

---
+++ Atividade14/script.js
@@ -0,0 +1,9 @@
+function maiusculas(){
+    var texto = document.getElementById("texto")
+    texto.value = texto.value.toUpperCase() 
+}
+
+function minusculas(){
+    var texto = document.getElementById("texto")
+    texto.value = texto.value.toLowerCase()
+}
